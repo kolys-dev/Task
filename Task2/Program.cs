9a8b0a7bf175b08b03d23e223c297b9f5a55e69f
Nikolay Nevolko <nikolay.nevolko@gmail.com>
2016-10-20 00:36:26 +0300

    com5

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -14,7 +14,6 @@ namespace Task2
 
         static void Main(string[] args)
         {
-
             numberOfStrings = int.Parse(args[1]);
             errorNum = int.Parse(args[2]);
 
@@ -23,17 +22,20 @@ namespace Task2
             string[] by_surnames_woman = System.IO.File.ReadAllLines(@"C:\Users\nikol\Desktop\data\data\by_surnames_woman.txt");
             string[] by_name_women = System.IO.File.ReadAllLines(@"C:\Users\nikol\Desktop\data\data\by_names_woman.txt");
             string[] by_zip = System.IO.File.ReadAllLines(@"C:\Users\nikol\Desktop\data\data\by_zip_codes.txt");
+            string[] by_streets = System.IO.File.ReadAllLines(@"C:\Users\nikol\Desktop\data\data\by_streets.txt");
 
             List<string> byzip = new List<string>();
             List<string> by_names_man = new List<string>();
             List<string> by_families_man = new List<string>();
             List<string> by_names_women = new List<string>();
             List<string> by_families_women = new List<string>();
+            List<string> by_streetes = new List<string>();
+
 
             bel_formatting(by_surnames_man, by_families_man);
             bel_formatting(by_zip, byzip);
             bel_formatting(by_name_man, by_names_man);
-
+            bel_formatting(by_streets, by_streetes);
             bel_formatting(by_surnames_woman, by_families_women);
             bel_formatting(by_name_women, by_names_women);
 
@@ -41,7 +43,7 @@ namespace Task2
             {
                 
                 case "by":
-                    output_bel(by_families_man, by_names_man, byzip, by_families_women, by_names_women);
+                    output_bel(by_families_man, by_names_man, byzip, by_families_women, by_names_women, by_streetes);
                     break;
 
                 case "ru":
@@ -68,21 +70,23 @@ namespace Task2
             }
         }
 
-        public static void output_bel(List<string> surname_man, List<string> name_man, List<string> zip, List<string> surname_women, List<string> name_women)
+        public static void output_bel(List<string> surname_man, List<string> name_man, List<string> zip, List<string> surname_women, List<string> name_women,
+             List<string> strets)
         {
+ 
             Random rnd = new Random();
             for (int i = 0; i <= numberOfStrings - 1; i++)
             {
                 if (rnd.Next(0, 2) == 1)
                 {
                     Console.WriteLine(surname_women[rnd.Next(surname_women.Count)] + " " + name_women[rnd.Next(name_women.Count)] +
-                        " " + zip[rnd.Next(zip.Count)]);
+                        " " + zip[rnd.Next(zip.Count)] + " " + strets[rnd.Next(strets.Count)] + " " + "+37529" + rnd.Next(1000000,9999999));
                 }
 
                 else
                 {
                     Console.WriteLine(surname_man[rnd.Next(surname_man.Count )] + " " + name_man[rnd.Next(name_man.Count)] +
-                       " " + zip[rnd.Next(zip.Count)]);
+                       " " + zip[rnd.Next(zip.Count)] + " " + strets[rnd.Next(strets.Count)] + " " + "+37529" + rnd.Next(1000000, 9999999));
                 }
             }
         }
